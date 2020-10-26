1da89bcb825a081781ae78013e8d6e58ebad501b
zhuoyuhe <zhuoyuhe@seas.upenn.edu>
2020-10-25 20:32:47 -0400

    change result type

--- src/lib/dataset/datasets/kitti_tracking_modified.py
+++ src/lib/dataset/datasets/kitti_tracking_modified.py
@@ -59,11 +59,42 @@ class KITTITracking_m(GenericDataset):
       f = open(out_path, 'w')
       images = self.video_to_images[video_id]
 
-      for image_info in images:
-        img_id = image_info['id']
-        if not (img_id in results):
-          continue
-        frame_id = image_info['frame_id']
+      # for image_info in images:
+      #   img_id = image_info['id']
+      #   if not (img_id in results):
+      #     continue
+      #   frame_id = image_info['frame_id']
+      #   for i in range(len(results[img_id])):
+      #     item = results[img_id][i]
+      #     category_id = item['class']
+      #     cls_name_ind = category_id
+      #     class_name = self.class_name[cls_name_ind - 1]
+      #     if not ('alpha' in item):
+      #       item['alpha'] = -1
+      #     if not ('rot_y' in item):
+      #       item['rot_y'] = -10
+      #     if 'dim' in item:
+      #       item['dim'] = [max(item['dim'][0], 0.01),
+      #         max(item['dim'][1], 0.01), max(item['dim'][2], 0.01)]
+      #     if not ('dim' in item):
+      #       item['dim'] = [-1, -1, -1]
+      #     if not ('loc' in item):
+      #       item['loc'] = [-1000, -1000, -1000]
+      #     f.write('{} 0.0 0'.format(class_name))
+      #     f.write(' {:d}'.format(int(item['alpha'])))
+      #     f.write(' {:.2f} {:.2f} {:.2f} {:.2f}'.format(
+      #       item['bbox'][0], item['bbox'][1], item['bbox'][2], item['bbox'][3]))
+      #
+      #     f.write(' {:d} {:d} {:d}'.format(
+      #       int(item['dim'][0]), int(item['dim'][1]), int(item['dim'][2])))
+      #     f.write(' {:d} {:d} {:d}'.format(
+      #       int(item['loc'][0]), int(item['loc'][1]), int(item['loc'][2])))
+      #     f.write(' {:d} {:.2f}\n'.format(int(item['rot_y']), item['score']))
+      # f.close()
+
+      for img_id in results.keys():
+        out_path = os.path.join(results_dir, '{:06d}.txt'.format(img_id))
+        f = open(out_path, 'w')
         for i in range(len(results[img_id])):
           item = results[img_id][i]
           category_id = item['class']
@@ -72,25 +103,25 @@ class KITTITracking_m(GenericDataset):
           if not ('alpha' in item):
             item['alpha'] = -1
           if not ('rot_y' in item):
-            item['rot_y'] = -10
+            item['rot_y'] = -1
           if 'dim' in item:
             item['dim'] = [max(item['dim'][0], 0.01),
-              max(item['dim'][1], 0.01), max(item['dim'][2], 0.01)]
+                           max(item['dim'][1], 0.01), max(item['dim'][2], 0.01)]
           if not ('dim' in item):
-            item['dim'] = [-1, -1, -1]
+            item['dim'] = [-1000, -1000, -1000]
           if not ('loc' in item):
             item['loc'] = [-1000, -1000, -1000]
           f.write('{} 0.0 0'.format(class_name))
-          f.write(' {:d}'.format(int(item['alpha'])))
+          f.write(' {:.2f}'.format(item['alpha']))
           f.write(' {:.2f} {:.2f} {:.2f} {:.2f}'.format(
             item['bbox'][0], item['bbox'][1], item['bbox'][2], item['bbox'][3]))
 
-          f.write(' {:d} {:d} {:d}'.format(
-            int(item['dim'][0]), int(item['dim'][1]), int(item['dim'][2])))
-          f.write(' {:d} {:d} {:d}'.format(
-            int(item['loc'][0]), int(item['loc'][1]), int(item['loc'][2])))
-          f.write(' {:d} {:.2f}\n'.format(int(item['rot_y']), item['score']))
-      f.close()
+          f.write(' {:.2f} {:.2f} {:.2f}'.format(
+            item['dim'][0], item['dim'][1], item['dim'][2]))
+          f.write(' {:.2f} {:.2f} {:.2f}'.format(
+            item['loc'][0], item['loc'][1], item['loc'][2]))
+          f.write(' {:.2f} {:.2f}\n'.format(item['rot_y'], item['score']))
+        f.close()
 
   def run_eval(self, results, save_dir, tasks=None):
     self.save_results(results, save_dir, tasks)
