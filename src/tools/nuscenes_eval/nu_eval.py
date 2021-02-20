eebfa0a5d511032791e7da105908e06d990f8b6a
zhuoyuhe <zhuoyuhe1583@outlook.com>
2021-02-20 12:19:49 +0800

    "fix nu_eval.py"

--- src/tools/nuscenes_eval/nu_eval.py
+++ src/tools/nuscenes_eval/nu_eval.py
@@ -2,6 +2,7 @@ import pycocotools.coco as coco
 from pycocotools.cocoeval import COCOeval
 # import torch
 # import torch.utils.data as data
+import argparse
 import json
 _valid_ids  = [
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -33,17 +34,21 @@ def convert_eval_format(all_bboxes):
     return detections
 
 if __name__ == '__main__':
-    path = 'C:/Users/zhuoyuhe/Desktop/incepio/CenterTrack/exp/results/'
-    coco = coco.COCO(path + "val.json")
-    with open(path + 'fadnet_no_share_45/save_results_nuscenes.json') as f:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gt_path', type=str,
+                        default='')
+    parser.add_argument('--result_path', type=str,
+                        default='')
+    opt = parser.parse_args()
+    # path = 'C:/Users/zhuoyuhe/Desktop/incepio/CenterTrack/exp/results/'
+    coco = coco.COCO(opt.gt_path)
+    with open(opt.result_path + '/save_results_nuscenes.json') as f:
         result = json.load(f)
     det = convert_eval_format(result)
     json.dump(det,
-              open('{}fadnet_no_share_45/results_coco.json'.format(path), 'w'))
-    coco_dets = coco.loadRes(path + 'fadnet_no_share_45/results_coco.json')
+              open('{}/results_coco.json'.format(opt.result_path), 'w'))
+    coco_dets = coco.loadRes(opt.result_path + '/results_coco.json')
     coco_eval = COCOeval(coco, coco_dets, "bbox")
     coco_eval.evaluate()
     coco_eval.accumulate()
     coco_eval.summarize()
-    a =1
-    b =2
